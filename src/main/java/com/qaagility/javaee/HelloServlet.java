8a487901c09180a655aa81e7317ef29364959212
devopsuser <devops@lntinfotech.com>
2017-06-18 16:10:11 -0700

    Version 0.2
    commit

--- src/main/java/com/qaagility/javaee/HelloServlet.java
+++ src/main/java/com/qaagility/javaee/HelloServlet.java
@@ -29,7 +29,7 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		String yourName = request.getParameter("yourName a");
+		String yourName = request.getParameter("yourName");
 		PrintWriter writer = response.getWriter();
 		writer.println("<h1>Hello " + yourName + "</h1>");
 		writer.close();
